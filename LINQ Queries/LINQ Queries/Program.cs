c3107839733d5c18bda2a5c034b397abb8f7a3fb
Rushikesh Naikwadi <rushee.mailme@gmail.com>
2024-05-05 14:08:54 +0530

    IEnumerable, IQueryable

--- LINQ Queries/LINQ Queries/Program.cs
+++ LINQ Queries/LINQ Queries/Program.cs
@@ -1,4 +1,12 @@
-﻿internal class Program
+﻿class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Salary { get; set; }
+}
+
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -31,6 +39,68 @@
         foreach (var item in mxresult)
         {
             Console.WriteLine(item);
+        }
+
+        Console.WriteLine("----------------------------------------");
+
+        List<Employee> emp = new List<Employee>()
+        {
+            new Employee()
+            {
+                Id = 1,
+                Name = "Sachin",
+                Salary = 106000,
+            },
+            new Employee()
+            {
+                Id = 2,
+                Name = "Amol",
+                Salary = 20000
+            },
+            new Employee()
+            {
+                Id = 3,
+                Name = "Tejas",
+                Salary = 0
+            },
+            new Employee()
+            {
+                Id = 4,
+                Name = "Akash",
+                Salary = 0
+            },
+            new Employee()
+            {
+                Id = 5,
+                Name = "Shubham",
+                Salary = 16000
+            },
+            new Employee()
+            {
+                Id = 6,
+                Name = "Rushi",
+                Salary = 37000
+            }
+        };
+
+        IEnumerable<Employee> filter = (from rec in emp
+                                       where rec.Salary != 0
+                                       select rec);
+
+        foreach (var item in filter)
+        {
+            Console.WriteLine(string.Format("Id : {0}, Name : {1}, Salary : {2}", item.Id, item.Name, item.Salary));
+        }
+
+        Console.WriteLine("----------------------------------------");
+
+        IQueryable<Employee> filter1 = (from rec in emp
+                                        where rec.Salary == 0
+                                        select rec).AsQueryable();
+
+        foreach (var item in filter1)
+        {
+            Console.WriteLine(string.Format("Id : {0}, Name : {1}, Salary : {2}", item.Id, item.Name, item.Salary));
         }
     }
 }
